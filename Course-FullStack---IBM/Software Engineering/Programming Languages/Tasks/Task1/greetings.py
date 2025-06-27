151a0fd00bc13054003b4198248bf3af4325e9aa
Max Vilà Ruiz <max16506@gmail.com>
2025-06-27 19:11:31 +0200

    FS-IBM: SWE - PL < Task1

---
+++ Course-FullStack---IBM/Software Engineering/Programming Languages/Tasks/Task1/greetings.py
@@ -0,0 +1,1 @@
+print("Hello Max! Good Evening")
---
+++ Course-FullStack---IBM/Software Engineering/Programming Languages/Tasks/Task1/oneliners.php
@@ -0,0 +1,5 @@
+<?php
+Print "Ens saciarem dels fruits del que diem i rebrem la paga del que fem.\n";
+Print "El neci es diverteix obrant amb infàmia; l'home intel·ligent, obrant amb saviesa.\n";
+Print "L'intel·ligent cerca coneixements, el savi mira d'escoltar-ne més.\n";
+?>
