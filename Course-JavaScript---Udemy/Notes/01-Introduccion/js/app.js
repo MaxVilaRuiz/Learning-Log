06bf2d343bbafa7499898f0a4eac5e8db904aeb3
Max Vilà Ruiz <max16506@gmail.com>
2025-06-27 16:52:28 +0200

    JS-Udemy: Notes - Introduction

---
+++ Course-JavaScript---Udemy/Notes/01-Introduccion/js/app.js
@@ -0,0 +1,11 @@
+//Los ";" sirven para indicar que ha finalizado la linea pero no es necesario (aunque si recomendable) a no ser que quieras programar varios elementos en la misma linea: console.log('Hola mundo'); console.log('Hola mundo')
+// const nombre = prompt('Cual es tu nombre'); //Input para preguntar al usuario (const = crear variable que no varia durante la ejecución del sistema)
+// document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo JavaScript moderno`; //Toma el nombre y lo muestra por pantalla
+// const producto = 'Monitor 24 pulgadas'; //Asigna el valor hacia la variable "producto"
+
+console.log('Hola mundo');
+function hola() {
+    console.log('Hola mundo') //Para escribir rapidamente 'console.log()' = clg + tab
+    console.log('Hola')
+    console.log('mundo')
+}
