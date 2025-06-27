c445c3b1b283ff972dbe93ecfc886e7f9af9a8a4
Max Vilà Ruiz <max16506@gmail.com>
2025-06-27 16:52:54 +0200

    JS-Udemy: Notes - Variables

---
+++ Course-JavaScript---Udemy/Notes/02-Variables/js/03-app.js
@@ -0,0 +1,9 @@
+//"const" sirve para crear variables fijas no generales (como "let") pero, no puede ser reasignadas
+// const producto = 'Tablet';
+// producto = 'Monitor';
+// console.log(producto);
+
+//Las variables con "const" deben empezar assignadas, no puedes no asignarles valor
+// const precio;
+// precio = 20;
+// console.log(precio);
