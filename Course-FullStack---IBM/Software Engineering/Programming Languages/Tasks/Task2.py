3d5b802ea56fd0b825377407c8fe4ff4030b31bb
Max Vilà Ruiz <max16506@gmail.com>
2025-06-27 19:11:50 +0200

    FS-IBM: SWE - PL < Task2

---
+++ Course-FullStack---IBM/Software Engineering/Programming Languages/Tasks/Task2.py
@@ -0,0 +1,44 @@
+#Ex 1:
+print('Hello World')
+
+#Ex 2:
+myint = 200
+print(myint)
+
+#Ex 3:
+a = 20
+c = 100 * a
+print(c)
+
+#Ex 4:
+myval = 8
+if myval >= 0:
+    print ('It is a positive number')
+else:
+    print('It is a negative number')
+
+#Ex 5:
+myarray = [20, 10, 44, 928, 200]
+for x in myarray:
+    print(x)
+
+#Ex 6:
+shoping_list = ['bread', 'water', 'chips', 'candy', 'soda']
+for x in shoping_list:
+    print(x)
+    if x == 'chips':
+        break
+
+#Ex 7:
+Score = 60
+while Score < 90:
+    print(Score)
+    Score += 5
+
+#Ex 8:
+myval2 = 45
+while myval2 < 90:
+    print(myval2)
+    if 80 < myval2:
+        break
+    myval2 += 8
