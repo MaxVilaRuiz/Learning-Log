7656d1c05c0c6c67b8700a1caa7605a66e737abd
Max Vilà Ruiz <max16506@gmail.com>
2025-07-01 13:31:36 +0200

    FS-IBM: Node.js - Callbacks

---
+++ Course-FullStack---IBM/Node.js & Express/Callback programming/Callback functions.js
@@ -0,0 +1,32 @@
+// Passing an error object
+exports.current = function(location, resultCallback) {
+    // ...
+    http.request(options, function(response) {
+        let buffer = '';
+        let result = '';
+
+        response.on('data', function(chunk) {
+            buffer += chunk;
+        });
+
+        response.on('end', function() {
+            parseString(buffer, function(error, result) {
+                if (error) {
+                    resultCallback(error);
+                    return;
+                }
+                resultCallback(null, result.current_observation.temp_f[0]);
+            });
+        });
+    });
+}
+
+// Callback function
+weather.current(location, function(error, temp_f) { // Assign an error parameter to check if an error has occurred
+    if (error) {
+        console.error(error);
+        return;
+    }
+    console.log(temp_f);
+});
+response.end(`... ${temp_f} ...`);
---
+++ Course-FullStack---IBM/Node.js & Express/Callback programming/Notes.js
@@ -0,0 +1,30 @@
+// Structure of callback functions
+// http.request(options, [callback function]);
+
+// Example
+http.request(options, function(response){});
+
+
+// Handling response events
+http.request(options, function(response) {
+    let buffer = '';
+
+    // object.on() is a function that defines an event handler that the framework calls when an event occurs
+    // Response object emits a 'data' event when Node.js module receives a part of the HTTP response message
+    response.on('data',function(chunk) {
+        buffer += chunk;
+    })
+
+    // When Node.js module receives the last part of the HTTP response message, the response object emits and 'end' event
+    response.on('end',function() {
+        console.log(buffer);
+    })
+}).end(); 
+
+
+// Handling error events
+let request = http.request(options,function(response){});
+request.on('error',function(e){ // 'error' to emit the appropiate execution if an error has occurred
+    resultCallback(e.message);
+});
+request.end(); 
