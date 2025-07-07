f3f4e68107c7618eb8d5cbf614782575f0b2886d
Max Vilà Ruiz <max16506@gmail.com>
2025-07-07 16:18:31 +0200

    Flutter-IBM: Navigation methods

---
+++ Course-Flutter---IBM/Navs.dart
@@ -0,0 +1,13 @@
+void navs() {
+  // Stack navigation
+  //  - The next screen is moved on top of the previous (ex: shopping app)
+  // Navigator.push(
+  //   context,
+  //   MaterialPageRoute(builder: (context) => ProductDetailScreen(product: product),);
+  // )
+
+  // Tab navigation
+  //  - To switch rapidly between screens (ex: homepage -> profile -> search page...)
+
+  // Drawer navigation (traditional sidebar / navbar)
+}
